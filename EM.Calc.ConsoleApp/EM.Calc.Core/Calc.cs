1aee3c3e28a391c7bdbbf0922c7d6e69519b2810
Sergey Sherstnev <sergeyshe2007@yandex.ru>
2018-10-29 21:55:14 +0300

    ДЗ №1 выполнено.

--- EM.Calc.ConsoleApp/EM.Calc.Core/Calc.cs
+++ EM.Calc.ConsoleApp/EM.Calc.Core/Calc.cs
@@ -12,5 +12,26 @@ namespace EM.Calc.Core
         {
             return args.Sum();
         }
+        public int Raz(int[] args)
+        {
+            int i;
+            i = args[0];
+            for (int ii = 1; ii < args.Length; ii++)
+            {
+                i-=args[ii];
+            }                              
+            return i ;
+        }
+        public int Stp(int[] args)
+        {
+            Double x;
+            x = args [0];
+            for (int ii = 1; ii < args.Length; ii++)
+            {
+                x = Math.Pow (x,args [ii]);
+            }
+            return (int)x;
+        }
     }
+
 }
--- EM.Calc.Tests/CalcTest.cs
+++ EM.Calc.Tests/CalcTest.cs
@@ -13,13 +13,17 @@ namespace EM.Calc.Tests
             //arrange
             var calc = new Core.Calc();
             var sum = 10;
+            var raz = 0;
+            var stp = 15625;
 
             //act
-            var result = calc.Sum(new[] { 5, 3, 2 });
-
+            var result1 = calc.Sum(new[] { 5, 3, 2 });
+            var result2 = calc.Raz(new[] { 5, 3, 2 });
+            var result3 = calc.Stp(new[] { 5, 3, 2 });
             //assert
-            Assert.AreEqual(sum, result);
-
+            Assert.AreEqual(sum, result1);
+            Assert.AreEqual(raz, result2);
+            Assert.AreEqual(stp, result3);
 
         }
     }
