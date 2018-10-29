884666cde2cd0c6a01c54724505ff6f8e0781a44
Sergey Sherstnev <sergeyshe2007@yandex.ru>
2018-10-29 16:46:44 +0300

    Добавил тесты с ядром

---
+++ EM.Calc.ConsoleApp/EM.Calc.Core/Calc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EM.Calc.Core
+{
+    public class Calc
+    {
+        public int Sum(int[] args)
+        {
+            return args.Sum();
+        }
+    }
+}
---
+++ EM.Calc.Tests/CalcTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EM.Calc.Tests
+{
+    [TestClass]
+    public class CalcTest
+    {
+        [TestMethod]
+        public void TestSum()
+        {
+
+            //arrange
+            var calc = new Core.Calc();
+            var sum = 10;
+
+            //act
+            var result = calc.Sum(new[] { 5, 3, 2 });
+
+            //assert
+            Assert.AreEqual(sum, result);
+
+
+        }
+    }
+}
